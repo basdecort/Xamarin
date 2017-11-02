c6bbae0c426527520cb8e8305672ff95e0f4f281
Bas de Cort <bas.decort@infosupport.com>
2017-11-02 15:26:10 +0100

    Added ToDo app

---
+++ ToDo/ToDo/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ToDo
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
