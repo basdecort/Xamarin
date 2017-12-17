84ba51ebfc3dc8e67c9f1cf2359bb8eda11198ba
Bas de Cort <bas.decort@infosupport.com>
2017-12-17 09:09:08 +0100

    Added SkiaSharp project with some basic drawing

---
+++ SkiaSample/SkiaSample/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace SkiaSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SkiaSamplePage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ SkiaSample/SkiaSample/SkiaSamplePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SkiaSharp;
+using Xamarin.Forms;
+
+namespace SkiaSample
+{
+    public partial class SkiaSamplePage : ContentPage
+    {
+        public SkiaSamplePage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs e)
+        {
+            var surface = e.Surface;
+            var canvas = surface.Canvas;
+
+            canvas.DrawCircle(0,0,100, new SKPaint{ Style = SkiaSharp.SKPaintStyle.Fill, Color = SKColors.Black });
+        }
+    }
+}
