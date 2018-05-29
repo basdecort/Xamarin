9bec20b43aafc1c21023a1dbeadaee72ae770981
Bas de Cort <bas.decort@infosupport.com>
2018-05-29 21:42:07 +0200

    Add Flexbox and CSS sample

---
+++ CSSAndFlexLayout/CSSAndFlexLayout/StyledPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CSSAndFlexLayout
+{
+    public partial class StyledPage : ContentPage
+    {
+        public StyledPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
