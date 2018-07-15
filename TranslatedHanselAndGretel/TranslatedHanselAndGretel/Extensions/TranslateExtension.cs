e867e0589585bcf0d526643ed55bd89e8a2a831d
Bas de Cort <bas.decort@infosupport.com>
2018-07-15 11:38:57 +0200

    Add translations project

---
+++ TranslatedHanselAndGretel/TranslatedHanselAndGretel/Extensions/TranslateExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TranslatedHanselAndGretel.Extensions
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return null;
+            
+            return TranslatedHanselAndGretel.Resources.Resources.ResourceManager.GetString(Text, CultureInfo.CurrentCulture);
+        }
+    }
+}
