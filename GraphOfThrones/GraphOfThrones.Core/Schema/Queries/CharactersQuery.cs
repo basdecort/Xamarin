b46ab691aebdbb575a34d0ee48ef531b30bc5dc5
Bas de Cort <bas.decort@infosupport.com>
2019-10-17 22:38:02 +0200

    Fix merge issues

--- GraphOfThrones/GraphOfThrones.Core/Schema/Queries/CharactersQuery.cs
+++ GraphOfThrones/GraphOfThrones.Core/Schema/Queries/CharactersQuery.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using GraphOfThrones.Core.Models;
 using GraphOfThrones.Core.Schema.Types;
 using GraphOfThrones.Core.Services;
 using GraphQL.Types;
@@ -7,10 +8,11 @@ namespace GraphOfThrones.Core.Schema.Queries
 {
     public class CharactersQuery : ObjectGraphType<object>
     {
-        public CharactersQuery(ICharacterService characterService)
+        public CharactersQuery(IService<Character> characterService, IService<Episode> episodeService)
         {
             Name = "Query";
             Field<ListGraphType<CharacterType>>("characters", resolve: (context) => characterService.GetAll());
+            Field<ListGraphType<CharacterType>>("episodes", resolve: (context) => episodeService.GetAll());
         }
     }
 }
